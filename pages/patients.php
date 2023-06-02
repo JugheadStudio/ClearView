e6bcd212f81b279f0a0cbcaadb8023eb352dabf6
Jugz <55399963+JugheadStudio@users.noreply.github.com>
2023-06-02 23:37:46 +0200

    data saved on create new patient, but image not uploaded

--- pages/patients.php
+++ pages/patients.php
@@ -46,13 +46,91 @@ include '../db.php';
 									<label for='profilePicture<?php echo $row['id']; ?>' class='form-label'>Profile Picture</label>
 									<input type='text' class='form-control' id='profilePicture<?php echo $row['id']; ?>' value='<?php echo $row['profilePicture']; ?>'>
 								</div>
-								<div class='mb-3'>
-									<label for='name<?php echo $row['id']; ?>' class='form-label'>Name</label>
-									<input type='text' class='form-control' id='name<?php echo $row['id']; ?>' value='<?php echo $row['name']; ?>'>
+								<div class='row mb-3'>
+									<div class="col-6">
+										<label for='name<?php echo $row['id']; ?>' class='form-label'>Name</label>
+										<input type='text' class='form-control' id='name<?php echo $row['id']; ?>' value='<?php echo $row['name']; ?>'>
+									</div>
+
+									<div class="col-6">
+										<label for='surname<?php echo $row['id']; ?>' class='form-label'>Surname</label>
+										<input type='text' class='form-control' id='surname<?php echo $row['id']; ?>' value='<?php echo $row['surname']; ?>'>
+									</div>
 								</div>
+
+								<div class='row mb-3'>
+									<div class="col-6">
+										<label for='dateOfBirth<?php echo $row['id']; ?>' class='form-label'>Date of Birth</label>
+										<input type='date' class='form-control' id='dateOfBirth<?php echo $row['id']; ?>' value='<?php echo $row['dateOfBirth']; ?>'>
+									</div>
+
+									<div class="col-6">
+										<label for="gender<?php echo $row['id']; ?>" class="form-label">Gender</label>
+										<select class="form-select" id="gender<?php echo $row['id']; ?>" name="gender">
+											<option value="Male" <?php if ($row['gender'] == 'Male') echo ' selected'; ?>>Male</option>
+											<option value="Female" <?php if ($row['gender'] == 'Female') echo ' selected'; ?>>Female</option>
+											<option value="Other" <?php if ($row['gender'] == 'Other') echo ' selected'; ?>>Other</option>
+										</select>
+									</div>
+
+								</div>
+
+								<div class='row mb-3'>
+									<div class="col-6">
+										<label for='phoneNumber<?php echo $row['id']; ?>' class='form-label'>Contact Number</label>
+										<input type='text' class='form-control' id='phoneNumber<?php echo $row['id']; ?>' value='<?php echo $row['phoneNumber']; ?>'>
+									</div>
+
+									<div class="col-6">
+										<label for='email<?php echo $row['id']; ?>' class='form-label'>Email</label>
+										<input type='email' class='form-control' id='email<?php echo $row['id']; ?>' value='<?php echo $row['email']; ?>'>
+									</div>
+								</div>
+
+								<div class='row mb-3'>
+									<div class="col-6">
+										<label for='medicalAid<?php echo $row['id']; ?>' class='form-label'>Medical Aid</label>
+										<input type='text' class='form-control' id='medicalAid<?php echo $row['id']; ?>' value='<?php echo $row['medicalAid']; ?>'>
+									</div>
+
+									<div class="col-6">
+										<label for='medicalAidNumber<?php echo $row['id']; ?>' class='form-label'>Medical Aid Number</label>
+										<input type='text' class='form-control' id='medicalAidNumber<?php echo $row['id']; ?>' value='<?php echo $row['medicalAidNumber']; ?>'>
+									</div>
+								</div>
+
+								<div class='row mb-3'>
+									<div class="col-6">
+										<label for="bloodType<?php echo $row['id']; ?>" class="form-label">Blood Type</label>
+										<select class="form-select" id="bloodType<?php echo $row['id']; ?>" name="bloodType">
+											<option value="" <?php if ($row['bloodType'] == '') echo 'selected'; ?>>Not Sure</option>
+											<option value="A" <?php if ($row['bloodType'] == 'A') echo 'selected'; ?>>A</option>
+											<option value="B" <?php if ($row['bloodType'] == 'B') echo 'selected'; ?>>B</option>
+											<option value="AB" <?php if ($row['bloodType'] == 'AB') echo 'selected'; ?>>AB</option>
+											<option value="O" <?php if ($row['bloodType'] == 'O') echo 'selected'; ?>>O</option>
+										</select>
+									</div>
+
+
+									<div class="col-6">
+										<label for='allergy<?php echo $row['id']; ?>' class='form-label'>Allergies</label>
+										<input type='text' class='form-control' id='allergy<?php echo $row['id']; ?>' value='<?php echo $row['allergy']; ?>'>
+									</div>
+								</div>
+
+								<div class='row mb-3'>
+									<div class="col-6">
+										<label for='emergencyContactName<?php echo $row['id']; ?>' class='form-label'>Emergency Contact</label>
+										<input type='text' class='form-control' id='emergencyContactName<?php echo $row['id']; ?>' value='<?php echo $row['emergencyContactName']; ?>'>
+									</div>
+
+									<div class="col-6">
+										<label for='emergencyContactNumber<?php echo $row['id']; ?>' class='form-label'>Emergency Contact Number</label>
+										<input type='text' class='form-control' id='emergencyContactNumber<?php echo $row['id']; ?>' value='<?php echo $row['emergencyContactNumber']; ?>'>
+									</div>
+								</div>
+
 								<div class='mb-3'>
-									<label for='surname<?php echo $row['id']; ?>' class='form-label'>Surname</label>
-									<input type='text' class='form-control' id='surname<?php echo $row['id']; ?>' value='<?php echo $row['surname']; ?>'>
 								</div>
 								<button type='button' class='btn btn-primary save-changes' data-entry-id='<?php echo $row['id']; ?>'>Save changes</button>
 							</form>
@@ -85,69 +163,91 @@ include '../db.php';
 			</div>
 			<div class='modal-body'>
 				<form id='addPatientForm' action='createPatients.php' method='POST'>
-					<div class='mb-3'>
-						<label for='id' class='form-label'>ID</label>
-						<input type='text' class='form-control' id='id' name='id' required>
+					<div class='row mb-3'>
+						<div class='col-md-6'>
+							<label for='id' class='form-label'>ID</label>
+							<input type='text' class='form-control' id='id' name='id' required>
+						</div>
+						<div class='col-md-6'>
+							<label for='profilePicture' class='form-label'>Profile Picture</label>
+							<input type='file' class='form-control' id='profilePicture' name='profilePicture' accept='image/*' required>
+						</div>
 					</div>
-					<div class='mb-3'>
-						<label for='profilePicture' class='form-label'>Profile Picture</label>
-						<input type='text' class='form-control' id='profilePicture' name='profilePicture' required>
+					<div class='row mb-3'>
+						<div class='col-md-6'>
+							<label for='name' class='form-label'>Name</label>
+							<input type='text' class='form-control' id='name' name='name' required>
+						</div>
+						<div class='col-md-6'>
+							<label for='surname' class='form-label'>Surname</label>
+							<input type='text' class='form-control' id='surname' name='surname' required>
+						</div>
 					</div>
-					<div class='mb-3'>
-						<label for='name' class='form-label'>Name</label>
-						<input type='text' class='form-control' id='name' name='name' required>
+					<div class='row mb-3'>
+						<div class='col-md-6'>
+							<label for='dateOfBirth' class='form-label'>Date of Birth</label>
+							<input type='date' class='form-control' id='dateOfBirth' name='dateOfBirth' required>
+						</div>
+						<div class='col-md-6'>
+							<label for='gender' class='form-label'>Gender</label>
+							<select class='form-select' id='gender' name='gender' required>
+								<option value='Male'>Male</option>
+								<option value='Female'>Female</option>
+								<option value='Other'>Other</option>
+							</select>
+						</div>
 					</div>
-					<div class='mb-3'>
-						<label for='surname' class='form-label'>Surname</label>
-						<input type='text' class='form-control' id='surname' name='surname' required>
+					<div class='row mb-3'>
+						<div class='col-md-6'>
+							<label for='phoneNumber' class='form-label'>Phone Number</label>
+							<input type='text' class='form-control' id='phoneNumber' name='phoneNumber' required>
+						</div>
+						<div class='col-md-6'>
+							<label for='email' class='form-label'>Email</label>
+							<input type='email' class='form-control' id='email' name='email' required>
+						</div>
 					</div>
-					<div class='mb-3'>
-						<label for='dateOfBirth' class='form-label'>Date of Birth</label>
-						<input type='date' class='form-control' id='dateOfBirth' name='dateOfBirth' required>
+					<div class='row mb-3'>
+						<div class='col-md-6'>
+							<label for='address' class='form-label'>Address</label>
+							<input type='text' class='form-control' id='address' name='address' required>
+						</div>
+						<div class='col-md-6'>
+							<label for='medicalAid' class='form-label'>Medical Aid</label>
+							<input type='text' class='form-control' id='medicalAid' name='medicalAid' required>
+						</div>
 					</div>
-					<div class='mb-3'>
-						<label for='gender' class='form-label'>Gender</label>
-						<select class='form-select' id='gender' name='gender' required>
-							<option value='Male'>Male</option>
-							<option value='Female'>Female</option>
-							<option value='Other'>Other</option>
-						</select>
+					<div class='row mb-3'>
+						<div class='col-md-6'>
+							<label for='medicalAidNumber' class='form-label'>Medical Aid Number</label>
+							<input type='text' class='form-control' id='medicalAidNumber' name='medicalAidNumber' required>
+						</div>
+						<div class='col-md-6'>
+							<label for='bloodType' class='form-label'>Blood Type</label>
+							<select class='form-select' id='bloodType' name='bloodType' required>
+								<option value='' selected>Not Sure</option>
+								<option value='A'>A</option>
+								<option value='B'>B</option>
+								<option value='AB'>AB</option>
+								<option value='O'>O</option>
+							</select>
+						</div>
 					</div>
-					<div class='mb-3'>
-						<label for='phoneNumber' class='form-label'>Phone Number</label>
-						<input type='text' class='form-control' id='phoneNumber' name='phoneNumber' required>
+					<div class='row mb-3'>
+						<div class='col-md-6'>
+							<label for='allergy' class='form-label'>Allergy</label>
+							<input type='text' class='form-control' id='allergy' name='allergy' required>
+						</div>
+						<div class='col-md-6'>
+							<label for='emergencyContactName' class='form-label'>Emergency Contact Name</label>
+							<input type='text' class='form-control' id='emergencyContactName' name='emergencyContactName' required>
+						</div>
 					</div>
-					<div class='mb-3'>
-						<label for='email' class='form-label'>Email</label>
-						<input type='email' class='form-control' id='email' name='email' required>
-					</div>
-					<div class='mb-3'>
-						<label for='address' class='form-label'>Address</label>
-						<input type='text' class='form-control' id='address' name='address' required>
-					</div>
-					<div class='mb-3'>
-						<label for='medicalAid' class='form-label'>Medical Aid</label>
-						<input type='text' class='form-control' id='medicalAid' name='medicalAid' required>
-					</div>
-					<div class='mb-3'>
-						<label for='medicalAidNumber' class='form-label'>Medical Aid Number</label>
-						<input type='text' class='form-control' id='medicalAidNumber' name='medicalAidNumber' required>
-					</div>
-					<div class='mb-3'>
-						<label for='bloodType' class='form-label'>Blood Type</label>
-						<input type='text' class='form-control' id='bloodType' name='bloodType' required>
-					</div>
-					<div class='mb-3'>
-						<label for='allergy' class='form-label'>Allergy</label>
-						<input type='text' class='form-control' id='allergy' name='allergy' required>
-					</div>
-					<div class='mb-3'>
-						<label for='emergencyContactName' class='form-label'>Emergency Contact Name</label>
-						<input type='text' class='form-control' id='emergencyContactName' name='emergencyContactName' required>
-					</div>
-					<div class='mb-3'>
-						<label for='emergencyContactNumber' class='form-label'>Emergency Contact Number</label>
-						<input type='text' class='form-control' id='emergencyContactNumber' name='emergencyContactNumber' required>
+					<div class='row mb-3'>
+						<div class='col-md-6'>
+							<label for='emergencyContactNumber' class='form-label'>Emergency Contact Number</label>
+							<input type='text' class='form-control' id='emergencyContactNumber' name='emergencyContactNumber' required>
+						</div>
 					</div>
 					<button type='submit' class='btn btn-primary'>Save</button>
 				</form>
@@ -158,6 +258,7 @@ include '../db.php';
 		</div>
 	</div>
 </div>
+
 
 
 <script src='https://code.jquery.com/jquery-3.6.0.min.js'></script>
--- updatePatients.php
+++ updatePatients.php
@@ -5,8 +5,19 @@ $id = $_POST['id'];
 $name = $_POST['name'];
 $surname = $_POST['surname'];
 $profilePicture = $_POST['profilePicture'];
+$dateOfBirth = $_POST['dateOfBirth'];
+$gender = $_POST['gender'];
+$phoneNumber = $_POST['phoneNumber'];
+$email = $_POST['email'];
+$address = $_POST['address'];
+$medicalAid = $_POST['medicalAid'];
+$medicalAidNumber = $_POST['medicalAidNumber'];
+$bloodType = $_POST['bloodType'];
+$allergy = $_POST['allergy'];
+$emergencyContactName = $_POST['emergencyContactName'];
+$emergencyContactNumber = $_POST['emergencyContactNumber'];
 
-$sql = "UPDATE `patients` SET `name`='$name',`surname`='$surname',`profilePicture`='$profilePicture' WHERE `id`='$id'";
+$sql = "UPDATE `patients` SET `id`='$id', `profilePicture`='$profilePicture', `name`='$name', `surname`='$surname', `dateOfBirth`='$dateOfBirth', `gender`='$gender', `phoneNumber`='$phoneNumber', `email`='$email', `address`='$address', `medicalAid`='$medicalAid', `medicalAidNumber`='$medicalAidNumber', `bloodType`='$bloodType', `allergy`='$allergy', `emergencyContactName`='$emergencyContactName', `emergencyContactNumber`='$emergencyContactNumber' WHERE `id`='$id'";
 
 if ($conn->query($sql) === TRUE) {
     echo 'Data updated successfully';
