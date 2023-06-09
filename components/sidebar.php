6b3cad053f5e1f8a8f1ed7a07e1169b2294f2370
Jugz <55399963+JugheadStudio@users.noreply.github.com>
2023-06-10 01:41:35 +0200

    test add and update for patients, doctors and receptionists
    Tested to see if I can create, update, delete doctors, patients and receptionists

--- components/sidebar.php
+++ components/sidebar.php
@@ -46,9 +46,11 @@ if (isset($_SESSION['username']) && isset($_SESSION['profilePicture'])) {
 
 		<div class="row">
 			<div class="col text-center">
-				<img src="<?php echo $profilePicture; ?>" alt="Profile Picture" width="65" height="65" class="rounded-circle logged-in-user-profile-picture mb-4" >
-				<p><?php echo $name; ?></p>
-				<button class="btn btn-outline-light" type="button" data-bs-toggle="modal" data-bs-target="#signOutModal">Sign out</button>
+				<img src="<?php echo $profilePicture; ?>" alt="Profile Picture" width="65" height="65" class="rounded-circle logged-in-user-profile-picture mb-3" >
+				<p class="m-0 text-sm">Logged in as</p>
+				<p class="mb-3"><?php echo $name; ?></p>
+				<button class="btn btn-outline-light mb-3" type="button" data-bs-toggle="modal" data-bs-target="#signOutModal">Log out</button>
+				<p class="poweredBy">Powered by<br><strong>Jughead Studio</strong></p>
 			</div>
 
 		</div>
@@ -62,11 +64,11 @@ if (isset($_SESSION['username']) && isset($_SESSION['profilePicture'])) {
 						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 					</div>
 					<div class="modal-body text-dark">
-						<p>Are you sure you want to sign out?</p>
+						<p>Are you sure you want to log out?</p>
 					</div>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-						<a href="pages/logout.php" class="btn btn-primary">Sign Out</a>
+						<a href="pages/logout.php" class="btn btn-primary">Log Out</a>
 					</div>
 				</div>
 			</div>
