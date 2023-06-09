11a0f82cc2423913f5125b30e73210e749205c58
Jugz <55399963+JugheadStudio@users.noreply.github.com>
2023-06-10 01:23:31 +0200

    implement password reset functionality
    Receptionists are now able to reset their passwords

--- pages/forgotPassword.php
+++ pages/forgotPassword.php
@@ -1,29 +1,138 @@
-<!-- forgotPassword.php -->
 <?php
-// Handle form submission
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Include the database connection and any required functions
+include '../db.php';
+
+// Check if the form is submitted
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Retrieve the username or email from the form submission
-    $usernameOrEmail = $_POST['usernameOrEmail'];
+  // Retrieve the username or email from the form
+  $usernameOrEmail = $_POST['usernameOrEmail'];
 
-    // Validate the provided username or email
-    // Check if the username or email exists in the database
+  // Validate the username or email (check if it exists in the database)
+  $sql = "SELECT * FROM receptionist WHERE username = ? OR email = ?";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param("ss", $usernameOrEmail, $usernameOrEmail);
+  $stmt->execute();
+  $result = $stmt->get_result();
 
-    // Generate a unique password reset token
+  if ($result->num_rows == 1) {
+    // User exists, generate a unique password reset token
+    $resetToken = generateResetToken();
 
-    // Store the token in the database along with the user's information
+    // Get the user's ID
+    $row = $result->fetch_assoc();
+    $userId = $row['id'];
+
+    // Store the token and the user's ID in the database
+    storeResetToken($userId, $resetToken);
 
     // Send the password reset email to the user
-    // Include the password reset link containing the token
-    // Example: $resetLink = "https://example.com/reset-password.php?token=<generated_token>";
+    $emailSent = sendPasswordResetEmail($row['email'], $resetToken);
 
-    // Redirect the user to a confirmation page
-    header('Location: forgot-password-confirmation.php');
+    if ($emailSent) {
+      // Display a success message
+      echo "Password reset email has been sent to your registered email address.";
+    } else {
+      // Display an error message if the email sending failed
+      echo "Failed to send the password reset email. Please try again later.";
+    }
     exit();
+  } else {
+    // User does not exist, display an error message
+    echo "User does not exist.";
+    exit();
+  }
 }
-?>
 
-<!-- HTML form for entering username or email -->
-<form method="POST" action="forgotPassword.php">
-    <input type="text" name="usernameOrEmail" placeholder="Username or Email" required>
-    <button type="submit">Reset Password</button>
+function generateResetToken()
+{
+  // Generate a random token using a combination of letters, numbers, and symbols
+  $token = bin2hex(random_bytes(32));
+
+  return $token;
+}
+
+function storeResetToken($userID, $resetToken)
+{
+  global $conn;
+
+  // Prepare the SQL statement
+  $sql = "INSERT INTO passwordResetTokens (userID, resetToken) VALUES (?, ?)";
+  $stmt = $conn->prepare($sql);
+
+  if (!$stmt) {
+    // Error in preparing the statement, handle it accordingly
+    die("Error: " . $conn->error);
+  }
+
+  // Bind the parameters and execute the statement
+  $stmt->bind_param("is", $userID, $resetToken);
+  $stmt->execute();
+
+  if ($stmt->affected_rows > 0) {
+    // Token stored successfully
+    return true;
+  } else {
+    // Failed to store the token
+    return false;
+  }
+}
+
+function sendPasswordResetEmail($email, $resetToken)
+{
+  // SendGrid API endpoint URL
+  $url = "https://api.sendgrid.com/v3/mail/send";
+
+  // SendGrid API key
+  $apiKey = "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";
+
+  // Compose the email data
+  $emailData = [
+    "personalizations" => [
+      [
+        "to" => [
+          [
+            "email" => $email
+          ]
+        ]
+      ]
+    ],
+    "from" => [
+      "email" => "150139@virtualwindow.co.za"
+    ],
+    "subject" => "Password Reset",
+    "content" => [
+      [
+        "type" => "text/html",
+        "value" => "Please click the following link to reset your password: <a href='http://localhost/ClearView/pages/resetPassword.php?token=$resetToken'>Reset Password</a>"
+      ]
+    ]
+  ];
+
+  // Send the email using SendGrid API
+  $curl = curl_init($url);
+  curl_setopt($curl, CURLOPT_POST, true);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($curl, CURLOPT_HTTPHEADER, [
+    "Authorization: Bearer $apiKey",
+    "Content-Type: application/json"
+  ]);
+  curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($emailData));
+  $response = curl_exec($curl);
+  curl_close($curl);
+
+  // Check the response and return true if the email is sent successfully
+  return $response !== false;
+}
+
+?>
+<!-- HTML form for requesting password reset -->
+<form action="forgotPassword.php" method="POST">
+  <div class="form-group">
+    <label for="usernameOrEmail">Username or Email:</label>
+    <input type="text" id="usernameOrEmail" name="usernameOrEmail" class="form-control" required>
+  </div>
+  <button type="submit" class="btn btn-primary">Reset Password</button>
 </form>
--- pages/resetPassword.php
+++ pages/resetPassword.php
@@ -1,23 +1,86 @@
-<!-- resetPassword.php -->
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Include the database connection and any required functions
+include '../db.php';
+
+// Retrieve the token from the URL parameter
+$resetToken = isset($_GET['token']) ? $_GET['token'] : '';
+
+
 // Handle form submission
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Retrieve the new password from the form submission
     $newPassword = $_POST['newPassword'];
 
-    // Retrieve the user's information from the database using the token
-
-    // Verify the token and check its expiration
-
-    // Update the user's password in the database
-
-    // Display a success message
-    echo "Your password has been successfully reset. You can now log in with your new password.";
-    exit();
+    if ($resetToken !== '') {
+      // Retrieve the user's information from the database using the token
+      $sql = "SELECT * FROM passwordResetTokens WHERE resetToken = ?";
+      $stmt = $conn->prepare($sql);
+      $stmt->bind_param("s", $resetToken);
+      $stmt->execute();
+      $result = $stmt->get_result();
+  
+      if ($stmt->errno) {
+          // Database query error
+          echo "Database query error: " . $stmt->error;
+          exit();
+      }
+  
+      if ($result->num_rows == 1) {
+          // Token exists, retrieve the user's ID
+          $row = $result->fetch_assoc();
+          $userId = $row['userID'];
+  
+          // Verify the token and check its expiration (implement your own logic)
+  
+          if ($row['resetToken'] === $resetToken) {
+              // Update the user's password in the database
+              $hashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
+              $sql = "UPDATE receptionist SET password = ? WHERE id = ?";
+              $stmt = $conn->prepare($sql);
+              $stmt->bind_param("si", $hashedPassword, $userId);
+              $stmt->execute();
+  
+              if ($stmt->errno) {
+                // Database update error
+                echo "Database update error: " . $stmt->error;
+                exit();
+            } elseif ($stmt->affected_rows > 0) {
+                // Password updated successfully
+                echo "Your password has been successfully reset. You can now log in with your new password.";
+                exit();
+            } else {
+                // Failed to update the password
+                echo "Failed to reset your password. Please try again later.";
+                exit();
+            }
+            
+          } else {
+              // Invalid token
+              echo "Invalid token. Please try again.";
+              exit();
+          }
+      } else {
+          // Invalid or expired token
+          echo "Invalid token. Please try again.";
+          exit();
+      }
+  } else {
+      // Token not provided
+      echo "Invalid token. Please try again.";
+      exit();
+  }
+  
+} else {
+    // No form submission, display the reset password form
+?>
+    <!-- HTML form for entering the new password -->
+    <form method="POST" action="resetPassword.php?token=<?php echo $resetToken; ?>">
+        <input type="password" name="newPassword" placeholder="New Password" required>
+        <button type="submit">Reset Password</button>
+    </form>
+<?php
 }
 ?>
-<!-- HTML form for entering the new password -->
-<form method="POST" action="resetPassword.php">
-    <input type="password" name="newPassword" placeholder="New Password" required>
-    <button type="submit">Reset Password</button>
-</form>
